ed6e0fd88fd6c5f1365f513f7d427b3d56a5cbc6
Sairam M <sairam.m.2005.65@gmail.com>
2025-04-11 18:04:44 +0530

    Update .gitignore, add Google Cloud Text-to-Speech dependency, and implement speech generation functions

--- gen.py
+++ gen.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import random
 import os
 import json
 
@@ -8,8 +9,13 @@ import gtts
 import pydub
 from pydub import AudioSegment
 import google.generativeai as genai
+import google.cloud.texttospeech as tts
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+speech_client = tts.TextToSpeechClient.from_service_account_json("gen-lang-client.json")
+
+VOICE_MODEL = "Wavenet" # "Standard" | "Wavenet" | "Chirp3"
 
 TTS_FOLDER = "tts"
 FINAL_FOLDER = "finals" # Folder to save the final podcast
@@ -28,11 +34,35 @@ Basically have a interviewer and a speaker, the interviewer asks questions relev
 the topic in detail. 
 Keep the conversation simple and easy to understand for a 20 year old.
 The conversation should be engaging and interesting.
-The conversation should be around 2-5 minutes long.
+The conversation should be around 8 minutes long.
 If the time is given, the conversation should be around that time.
-If time given is more than 5 minutes, then the conversation should be around 5 minutes.
-If time given is less than 2 minutes, then the conversation should be around 2 minutes.
-The conversation should be in English.
+If time given is more than 8 minutes, then the conversation should be around 8 minutes.
+Include ahhs and umms in the conversation to make it sound more natural.
+The conversation should be in the format of a podcast.
+
+Around 20 questions and answers should be there in the conversation.
+
+Keep the following in mind:
+1. Keep the conversation simple and easy to understand.
+2. Use simple words and phrases.
+3. Keep things casual and friendly.
+4. Use contractions (e.g., "you're" instead of "you are").
+5. Remember that the audience is 20 year old engineering students.
+6. Use a friendly and casual tone.
+7. Use SSML tags to add pauses, emphasis, and other speech effects.
+8. If the word in the specified language is not available, use the English word.
+9. Make the conversation longer and deeper.
+10. If the question/statement seems too long, break it down into multiple consecutive questions/statements.
+
+Try different names for the interviewer and speaker.
+Use Dr. for the speaker name.
+
+Usually first names are used to address someone in India
+
+The target audience is 20 year old engineering students.
+
+Language: English (American)
+
 PODCAST SCHEMA:
 """
 
@@ -41,15 +71,25 @@ podcast_schema = """
     "podcastTitle": "Podcast Title",
     "podcastDescription": "Podcast Description",
     "episodeTitle": "Episode Title",
-    "interviewer": "Interviewer Name",
-    "speaker": "Speaker Name",
+    "interviewer": {
+        "name": "Interviewer Name",
+        "country": "Language Code in the format of 'en-US' or 'en-GB'",
+        "gender": "male | female",
+    },
+    "language": "Language Code in the format of 'en-US' or 'en-GB'",
+    "episodeNumber": "Episode Number",
+    "speaker": {
+        "name": "Speaker Name",
+        "country": "Language Code in the format of 'en-US' or 'en-GB'",
+        "gender": "male | female",
+    },
     "conversation": [
         {
-            "speaker": "interviewer | speaker",
-            "text": "Question or statement"
+            "speaker": "interviewer | speaker (enum)",
+            "text": "Question or statement",
         },
         {
-            "speaker": "interviewer | speaker",
+            "speaker": "interviewer | speaker (enum)",
             "text": "Question or statement"
         }
     ]
@@ -60,11 +100,34 @@ def generate_podcast(topic):
     response = model.generate_content(prompt.format(topic=topic) + podcast_schema, generation_config={"max_output_tokens": 4000})
     return response.text
 
-def save_podcast(text, filename, tld="com"):
-
+def save_podcast(text, filename, lang="en-IN", voice="en-IN-Standard-A"):
+    
     filename = os.path.join(TTS_FOLDER, filename)
-    tts = gtts.gTTS(text, tld=tld, slow=False)
-    tts.save(filename)
+    speech = speech_client.synthesize_speech(
+        # input=tts.SynthesisInput(text=text),
+        input=tts.SynthesisInput(ssml=f"<speak>{text}</speak>"),
+        voice=tts.VoiceSelectionParams(
+            language_code=lang,
+            name=voice,
+        ),
+        audio_config=tts.AudioConfig(
+            audio_encoding=tts.AudioEncoding.LINEAR16,
+            # speaking_rate=1.05,
+            pitch=0.0,
+        ),
+    )
+    with open(filename, "wb") as out:
+        out.write(speech.audio_content)
+    
+
+# def save_podcast(text, filename, tld="com"):
+#     filename = os.path.join(TTS_FOLDER, filename)
+#     tts = speech_model.generate_content(text, generation_config={"max_output_tokens": 4000})
+#     print(tts.text)
+#     # tts.save(filename)
+
+def remap_os_safe_title(title: str) -> str:
+    return title.replace(" ", "_").replace(":", "_").replace("?", "_").replace("!", "_").replace(",", "_")
 
 def main(topic="machine learning"):
     podcast = generate_podcast(topic)
@@ -75,44 +138,75 @@ def main(topic="machine learning"):
     interviewer = podcast["interviewer"]
     speaker = podcast["speaker"]
     conversation = podcast["conversation"]
-    os_safe_title = podcast_title.replace(" ", "_").replace(":", "_").replace("?", "_").replace("!", "_").replace(",", "_")
-    os_safe_interviewer = interviewer.replace(" ", "_").replace(":", "_").replace("?", "_").replace("!", "_").replace(",", "_")
-    os_safe_speaker = speaker.replace(" ", "_").replace(":", "_").replace("?", "_").replace("!", "_").replace(",", "_")
+    os_safe_title = remap_os_safe_title(podcast_title)
+    os_safe_interviewer = remap_os_safe_title(interviewer["name"])
+    os_safe_speaker = remap_os_safe_title(speaker["name"])
+    
+    country_interviewer = interviewer["country"]
+    country_speaker = speaker["country"]
+    gender_interviewer = interviewer["gender"]
+    gender_speaker = speaker["gender"]
+    language = podcast["language"]
+    episode_number = podcast["episodeNumber"]
+
+
+    # Filter voices by gender
+    resp_voices_inter = speech_client.list_voices(language_code=country_interviewer).voices
+    gen_voices_inter = [voice for voice in resp_voices_inter if tts.SsmlVoiceGender(voice.ssml_gender).name == gender_interviewer.upper() and VOICE_MODEL in voice.name]
+    gen_voice_inter = random.choice(gen_voices_inter)
+
+    resp_voices_speak = speech_client.list_voices(language_code=country_speaker).voices
+    gen_voices_speak = [voice for voice in resp_voices_speak if tts.SsmlVoiceGender(voice.ssml_gender).name == gender_speaker.upper() and VOICE_MODEL in voice.name and voice.name != gen_voice_inter.name]
+    gen_voice_speak = random.choice(gen_voices_speak)
+
+
+    resp_voices_ann = speech_client.list_voices(language_code=language).voices
+    gen_voices_ann = [voice for voice in resp_voices_ann if tts.SsmlVoiceGender(voice.ssml_gender).name == gender_speaker.upper() and VOICE_MODEL in voice.name and voice.name != gen_voice_inter.name and voice.name != gen_voice_speak.name]
+    gen_voice_ann = random.choice(gen_voices_ann)
+
+
+    print(f"Announcer voice: {gen_voice_ann.name} ({gen_voice_ann.language_codes})")
+    print(f"Interviewer voice: {gen_voice_inter.name} ({gen_voice_inter.language_codes})")
+    print(f"Speaker voice: {gen_voice_speak.name} ({gen_voice_speak.language_codes})")
+
     print("Generating podcast...")
     print(f"Podcast Title: {podcast_title}")
-    save_podcast(podcast_title, f"{os_safe_title}.mp3", tld="us")
-    save_podcast(podcast_description, f"{os_safe_title}_description.mp3", tld="us")
-    save_podcast(episode_title, f"{os_safe_title}_episode.mp3", tld="us")
+    save_podcast(podcast_title, f"{os_safe_title}.wav", country_interviewer, gen_voice_ann.name)
+    save_podcast(podcast_description, f"{os_safe_title}_description.wav", country_interviewer, gen_voice_ann.name)
+    save_podcast(f"{episode_number}. {episode_title}", f"{os_safe_title}_episode.wav", language, gen_voice_ann.name)
+    
     for idx, turn in enumerate(conversation):
         if turn["speaker"] == "interviewer":
-            save_podcast(turn["text"], f"{os_safe_title}_{os_safe_interviewer}_{idx}.mp3", tld="co.uk")
+            save_podcast(turn["text"], f"{os_safe_title}_{os_safe_interviewer}_{idx}.wav", country_interviewer, gen_voice_inter.name)
         else:
-            save_podcast(turn["text"], f"{os_safe_title}_{os_safe_speaker}_{idx}.mp3", tld="us")
+            save_podcast(turn["text"], f"{os_safe_title}_{os_safe_speaker}_{idx}.wav", country_speaker, gen_voice_speak.name)
+        print(f"Generated {turn['speaker']} audio for turn {idx + 1}/{len(conversation)}")
 
     print(f"Podcast '{podcast_title}' generated and saved in {os.path.abspath('tts')} folder.")
 
     # merge the files into a single podcast
-    podcast_files = [f"{os_safe_title}.mp3", f"{os_safe_title}_description.mp3", f"{os_safe_title}_episode.mp3"]
+    podcast_files = [f"{os_safe_title}.wav", f"{os_safe_title}_description.wav", f"{os_safe_title}_episode.wav"]
     for idx, turn in enumerate(conversation):
         if turn["speaker"] == "interviewer":
-            podcast_files.append(f"{os_safe_title}_{os_safe_interviewer}_{idx}.mp3")
+            podcast_files.append(f"{os_safe_title}_{os_safe_interviewer}_{idx}.wav")
         else:
-            podcast_files.append(f"{os_safe_title}_{os_safe_speaker}_{idx}.mp3")
+            podcast_files.append(f"{os_safe_title}_{os_safe_speaker}_{idx}.wav")
     
-    segments = [pydub.AudioSegment.from_mp3(os.path.join(TTS_FOLDER, file)) for file in podcast_files]
+    segments = [pydub.AudioSegment.from_wav(os.path.join(TTS_FOLDER, file)) for file in podcast_files]
     combined = AudioSegment.silent(duration=500)
     for segment in segments:
         combined += segment + AudioSegment.silent(duration=500)  # Add a 1-second silence between segments
     
-    final_file = os.path.join(FINAL_FOLDER, f"{os_safe_title}_final.mp3")
-    combined.export(final_file, format="mp3")
-    print(f"Final podcast '{podcast_title}_final.mp3' generated.")
+    final_file = os.path.join(FINAL_FOLDER, f"{os_safe_title}_final.wav")
+    combined.export(final_file, format="wav")
+    print(f"Final podcast '{podcast_title}_final.wav' generated.")
     return {
         "podcast_title": podcast_title,
         "podcast_description": podcast_description,
         "episode_title": episode_title,
         "interviewer": interviewer,
         "speaker": speaker,
+        "conversation": conversation,
         "audio_file": final_file
     }
 
---
+++ speech.py
@@ -0,0 +1,67 @@
+from typing import Sequence
+
+import google.cloud.texttospeech as tts
+
+
+
+def unique_languages_from_voices(voices: Sequence[tts.Voice]):
+    language_set = set()
+    for voice in voices:
+        for language_code in voice.language_codes:
+            language_set.add(language_code)
+    return language_set
+
+
+def list_languages():
+    client = tts.TextToSpeechClient.from_service_account_json("gen-lang-client.json")
+    response = client.list_voices()
+    languages = unique_languages_from_voices(response.voices)
+
+    print(f" Languages: {len(languages)} ".center(60, "-"))
+    for i, language in enumerate(sorted(languages)):
+        print(f"{language:>10}", end="\n" if i % 5 == 4 else "")
+
+import google.cloud.texttospeech as tts
+
+
+def list_voices(language_code=None):
+    client = tts.TextToSpeechClient.from_service_account_json("gen-lang-client.json")
+    response = client.list_voices(language_code=language_code)
+    voices = sorted(response.voices, key=lambda voice: voice.name)
+
+    print(f" Voices: {len(voices)} ".center(60, "-"))
+    for voice in voices:
+        languages = ", ".join(voice.language_codes)
+        name = voice.name
+        gender = tts.SsmlVoiceGender(voice.ssml_gender).name
+        rate = voice.natural_sample_rate_hertz
+        print(f"{languages:<8} | {name:<24} | {gender:<8} | {rate:,} Hz")
+        
+
+import google.cloud.texttospeech as tts
+
+
+def text_to_wav(voice_name: str, text: str):
+    language_code = "-".join(voice_name.split("-")[:2])
+    text_input = tts.SynthesisInput(text=text)
+    voice_params = tts.VoiceSelectionParams(
+        language_code=language_code, name=voice_name
+    )
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+
+    client = tts.TextToSpeechClient.from_service_account_json("gen-lang-client.json")
+    response = client.synthesize_speech(
+        input=text_input,
+        voice=voice_params,
+        audio_config=audio_config,
+    )
+
+    filename = f"{voice_name}.wav"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+        print(f'Generated speech saved to "{filename}"')
+        
+
+# list_voices()
+
+text_to_wav("en-GB-Standard-O", "Ah, like time-sharing! So the OS juggles running programs using these scheduling rules to decide who gets the CPU next. Cool!")
