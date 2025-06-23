2aa682bd8e2c912d7d719b918eded21a3f01a5a1
Sairam M <sairam.m.2005.65@gmail.com>
2025-06-23 20:56:49 +0530

    fix: Update database URL for async connection to Supabase

--- api/db.py
+++ api/db.py
@@ -5,8 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 # Make it async
 
-db_url = "postgresql+asyncpg://postgres:aipodcast-123@db.kzgbfmhlcmfjknkbvggg.supabase.co:5432/postgres"
-# db_url = "postgresql+asyncpg://postgres.kzgbfmhlcmfjknkbvggg:ai-podcast-123@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
+# db_url = "postgresql+asyncpg://postgres:aipodcast-123@db.kzgbfmhlcmfjknkbvggg.supabase.co:5432/postgres"
+db_url = "postgresql+asyncpg://postgres.kzgbfmhlcmfjknkbvggg:ai-podcast-123@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
 engine = create_async_engine(
     db_url,
     echo=True)
