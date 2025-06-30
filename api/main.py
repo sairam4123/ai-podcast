009422f9ec280df157e3c12a703848dd64222bc9
Sairam M <sairam.m.2005.65@gmail.com>
2025-07-01 00:34:30 +0530

    fix: Update CORS middleware to allow all HTTP methods

--- api/main.py
+++ api/main.py
@@ -51,7 +51,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=("*"),  # Allows all origins
     allow_credentials=True,
-    allow_methods=("GET", "POST", "PUT", "OPTIONS", "DELETE"),  # Allows all methods
+    allow_methods=("*"),  # Allows all methods
     allow_headers=("*"),  # Allows all headers
 
 )
