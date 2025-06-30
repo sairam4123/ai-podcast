0c147a4f492afa98b7c9bb4be55b2349d38a933d
Sairam M <sairam.m.2005.65@gmail.com>
2025-07-01 00:35:00 +0530

    fix: Disable allow_credentials in CORS middleware configuration

--- api/main.py
+++ api/main.py
@@ -50,7 +50,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=("*"),  # Allows all origins
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=("*"),  # Allows all methods
     allow_headers=("*"),  # Allows all headers
 
